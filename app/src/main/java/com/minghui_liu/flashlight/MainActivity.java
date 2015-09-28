65e26eaec7fca9ff357ef95547674da609786c9b
kevin <minghui.liu@trincoll.edu>
2015-09-28 12:14:09 -0400

    Initial commit

---
+++ app/src/main/java/com/minghui_liu/flashlight/MainActivity.java
@@ -0,0 +1,213 @@
+package com.minghui_liu.flashlight;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    ImageButton btnSwitch;
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    Camera.Parameters params;
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // flash switch button
+        btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlashOn) {
+                    turnOffFlash();
+                } else {
+                    turnOnFlash();
+                }
+            }
+        });
+
+        // get the camera
+        getCamera();
+
+        // display switch image
+        toggleButtonImage();
+
+        // check if device supports flash light
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!hasFlash) {
+            // device does not support flash light
+            // show alert window and exit application
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+            alert.setTitle("Oops");
+            alert.setMessage("It seems that your device does not support flash lisght : (");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // close the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // on pause turn off the flash
+        turnOffFlash();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // on resume turn on the flash
+        if (hasFlash && isFlashOn)
+            turnOnFlash();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // on starting the app get the camera params
+        getCamera();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // on stop release the camera
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Getting camera parameters
+     */
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Failed to open camera: ", e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Turn on flash light
+     */
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play button sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+
+            // change switch image
+            toggleButtonImage();
+        }
+    }
+
+    /**
+     * Turn off flash light
+     */
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+            isFlashOn = false;
+
+            // change switch image
+            toggleButtonImage();
+        }
+    }
+
+    /**
+     * Toggle switch button image
+     */
+    private void toggleButtonImage() {
+        if (isFlashOn) {
+            btnSwitch.setImageResource(R.drawable.btn_switch_on);
+        } else {
+            btnSwitch.setImageResource(R.drawable.btn_switch_off);
+        }
+    }
+
+    /**
+     * Play sound when switch button is clicked on
+     */
+    private void playSound() {
+        if (isFlashOn) {
+            mp = MediaPlayer.create(MainActivity.this, R.raw.light_switch_off);
+        } else {
+            mp = MediaPlayer.create(MainActivity.this, R.raw.light_switch_on);
+        }
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+        mp.start();
+    }
+
+}
